b04b49353aa2f226416f3314f929ab5d779c2955
Azgardclient <aslan051105@gmail.com>
2026-04-27 10:12:16 +0000

    move client code to client initializer

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,7 +1,11 @@
 package com.example;
+
 import net.fabricmc.api.ModInitializer;
 
 public class ExampleMod implements ModInitializer {
     @Override
-    public void onInitialize() {}
+    public void onInitialize() {
+        // Здесь можно оставить пусто или добавить лог
+        System.out.println("Azgard Mod Initialized!");
+    }
 }
