5e716992a729811503cd70ec563fdd49da36564d
Azgardclient <aslan051105@gmail.com>
2026-04-27 06:20:19 +0000

    final build

--- src/client/java/com/example/client/ExampleModClient.java
+++ src/client/java/com/example/client/ExampleModClient.java
@@ -1,19 +1,10 @@
 package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 
 public class ExampleModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.azgard.open", 
-            InputUtil.Type.KEYSYM, 
-            GLFW.GLFW_KEY_M, 
-            "category.azgard.client"
-        ));
+        System.out.println("Azgard Client Loaded!");
     }
 }
