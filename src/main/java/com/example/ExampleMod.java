1cd261b074164dad02525de4b268f0239479ef93
Azgardclient <aslan051105@gmail.com>
2026-04-27 08:20:18 +0000

    stable build

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -10,17 +10,4 @@ public class ExampleMod implements ModInitializer {
     @Override
     public void onInitialize() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (client.options.allKeys[77].wasPressed()) {
-                client.setScreen(new Screen(Text.literal("Azgard")) {
-                    @Override
-                    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-                        context.fill(width / 2 - 100, height / 2 - 70, width / 2 + 100, height / 2 + 70, 0xBB000000);
-                        context.fill(width / 2 - 100, height / 2 - 72, width / 2 + 100, height / 2 - 70, 0xFFFFFFFF);
-                        context.drawCenteredTextWithShadow(this.textRenderer, "AZGARD CLIENT", width / 2, height / 2 - 60, 0xFFFFFFFF);
-                        super.render(context, mouseX, mouseY, delta);
-                    }
-                });
-            }
-        });
-    }
-}
+while (.ptions.allKeyswasPressed
