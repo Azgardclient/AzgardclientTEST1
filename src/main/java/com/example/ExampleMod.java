43201708ae1d3d5db498938cf4b38844a505f7de
Azgardclient <aslan051105@gmail.com>
2026-04-27 08:25:23 +0000

    stable build

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,13 +1,27 @@
-package com.example;
-
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.client.gui.DrawContext;
 
-public class ExampleMod implements ModInitializer {
+public class MyCustomScreen extends Screen {
+    public MyCustomScreen() {
+        super(Text.literal("Мое Кастомное Меню"));
+    }
+
     @Override
-    public void onInitialize() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-while (.ptions.allKeyswasPressed
+    protected void init() {
+        // Добавляем кнопку
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("Нажми меня"), button -> {
+            System.out.println("Кнопка нажата!");
+        })
+        .dimensions(this.width / 2 - 100, this.height / 2, 200, 20) // Позиция и размер
+        .build());
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context, mouseX, mouseY, delta); // Затемнение заднего фона
+        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, 0xFFFFFF);
+        super.render(context, mouseX, mouseY, delta);
+    }
+}
