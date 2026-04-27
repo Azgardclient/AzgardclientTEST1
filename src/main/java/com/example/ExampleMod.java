a0273192f400ca55b62b7a671ce628a0b9201f14
Azgardclient <aslan051105@gmail.com>
2026-04-27 09:49:36 +0000

    final split fix

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,10 +1,7 @@
 package com.example;
-
 import net.fabricmc.api.ModInitializer;
 
 public class ExampleMod implements ModInitializer {
     @Override
-    public void onInitialize() {
-        // Здесь ничего не пишем, чтобы сервер не ругался
-    }
+    public void onInitialize() {}
 }
--- src/main/java/com/example/ExampleModClient.java
+++ src/main/java/com/example/ExampleModClient.java
@@ -14,9 +14,7 @@ public class ExampleModClient implements ClientModInitializer {
                 client.setScreen(new Screen(Text.literal("Azgard")) {
                     @Override
                     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-                        // Дизайн Azgard / Nursultan
                         context.fill(width / 2 - 100, height / 2 - 70, width / 2 + 100, height / 2 + 70, 0xBB000000);
-                        context.fill(width / 2 - 100, height / 2 - 72, width / 2 + 100, height / 2 - 70, 0xFFFFFFFF);
                         context.drawCenteredTextWithShadow(this.textRenderer, "AZGARD CLIENT", width / 2, height / 2 - 60, 0xFFFFFFFF);
                         super.render(context, mouseX, mouseY, delta);
                     }
