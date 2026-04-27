9da35f3fbb78dcdbeb2310f8e73be3856c4e9b94
Azgardclient <aslan051105@gmail.com>
2026-04-27 06:26:37 +0000

    finalV1

--- src/client/java/com/example/client/ExampleModClient.java
+++ src/client/java/com/example/client/ExampleModClient.java
@@ -1,10 +1,31 @@
 package com.example;
 
+import com.example.client.AzgardScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class ExampleModClient implements ClientModInitializer {
+    private static KeyBinding keyBinding;
+
     @Override
     public void onInitializeClient() {
-        System.out.println("Azgard Client Loaded!");
+        // Регистрация кнопки
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.azgard.open", 
+            InputUtil.Type.KEYSYM, 
+            GLFW.GLFW_KEY_M, 
+            "category.azgard.client"
+        ));
+
+        // Проверка нажатия каждый тик
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.wasPressed()) {
+                client.setScreen(new AzgardScreen());
+            }
+        });
     }
 }
